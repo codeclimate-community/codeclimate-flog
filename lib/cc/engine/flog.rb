19ddee7e1200581293bdd91b3924db1bd215cd21
Ryan Davis <ryand-ruby@zenspider.com>
2017-01-10 16:39:11 -0800

    Enhancements from code review w/ gordon:
    + Make `all` an option to toggle between reporting top 60% and everything.
    + Added documentation on methods.
    + Added remediation (just raw flog score for now).
    + Added fingerprint (md5 of just name for now).

--- lib/cc/engine/flog.rb
+++ lib/cc/engine/flog.rb
@@ -1,10 +1,17 @@
 require "path_expander"
 require "flog"
+require "digest/md5"
 
 module CC
   module Engine
     class Flog
       VERSION = "1.0.0"
+
+      # TODO: non-methods (ie, attrs/scopes/relations) have no location, skipped
+      # TODO: possibly scale remediation score exponentially
+      # TODO: XXX#main is locationless. improve flog to report first sighting?
+      # TODO: possibly add score to fingerprint?
+      # TODO: Finish runing through the QA spreadsheet
 
       attr_accessor :dir
       attr_accessor :config
@@ -13,39 +20,46 @@ module CC
       attr_accessor :files
 
       DEFAULTS = {
-                  "include_paths" => ["."]
+                  "include_paths" => ["."],
+                  "all"           => true, # users must opt-in
                  }
 
       def initialize(root, config = {}, io = STDOUT)
         self.dir    = root
-        self.config = DEFAULTS.merge config
+        self.config = config = DEFAULTS.merge config
         self.io     = io
-        self.flog   = ::Flog.new :all => true, :continue => true
+        self.flog   = ::Flog.new :all => config["all"], :continue => true
 
         paths = config["include_paths"].dup
         expander = PathExpander.new paths, "**/*.{rb,rake}"
         self.files = expander.process.select { |s| s =~ /\.(?:rb|rake)$/ }
       end
 
+      ##
+      # Run flog and print issues as they come up.
+
       def run
         Dir.chdir dir do
           flog.flog(*files)
 
-          flog.each_by_score do |name, score, call_list|
+          flog.each_by_score flog.threshold do |name, score, call_list|
             location = flog.method_locations[name]
 
             next unless location # XXX#main is location-less, skip for now
 
             datum = "%s scored %.1f" % [name, score]
+            issue = self.issue name, datum, location, score
 
-            warn issue(datum, location).inspect
-            io.print issue(datum, location).to_json
+            io.print issue.to_json
             io.print "\0"
           end
         end
       end
 
-      def issue datum, location
+      ##
+      # Create an issue hash from +name+, +datum+, +location+, and +score+.
+
+      def issue name, datum, location, score
         file, line = location.split(":", 2)
         line = line.to_i
         {
@@ -53,6 +67,8 @@ module CC
          "check_name"  => "Flog Score",
          "description" => datum,
          "categories"  => ["Complexity"],
+         "remediation" => score,
+         "fingerprint" => Digest::MD5.hexdigest(name),
          "location"    => {
                            "path"  => file,
                            "lines" => {"begin" => line, "end" => line}
