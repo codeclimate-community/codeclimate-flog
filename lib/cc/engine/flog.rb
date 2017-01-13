8fd28b449c04a8bdcd5b41159771ff292d51dbe6
Ryan Davis <ryand-ruby@zenspider.com>
2017-01-13 14:07:34 -0800

    Added normalize_conf to demangle quasi-yaml into real values.
    Added a BUNCH of tests for it so I can detect if/when it changes (I hope).

--- lib/cc/engine/flog.rb
+++ lib/cc/engine/flog.rb
@@ -26,13 +26,46 @@ module CC
 
       def initialize(root, config = {}, io = STDOUT)
         self.dir    = root
-        self.config = config = DEFAULTS.merge config
+        self.config = config = normalize_conf config
         self.io     = io
         self.flog   = ::Flog.new :all => config["all"], :continue => true
 
         paths = config["include_paths"].dup
         expander = PathExpander.new paths, "**/*.{rb,rake}"
         self.files = expander.process.select { |s| s =~ /\.(?:rb|rake)$/ }
+      end
+
+      ##
+      # The resulting config coming from codeclimate and/or docker is
+      # pretty mangled. It isn't actually parsing yaml properly and
+      # this deals with that.
+      #
+      # https://github.com/codeclimate/codeclimate-yaml/issues/38
+      # https://github.com/codeclimate/codeclimate-yaml/issues/39
+
+      def normalize_conf top_config
+        # fix the top, if necessary
+        top_config = reparse top_config
+        top_config["config"] = reparse(top_config["config"]) || {}
+
+        # normalize contents
+        config = DEFAULTS.merge top_config["config"]
+        config["include_paths"] = top_config["include_paths"] if
+          top_config["include_paths"]
+
+        # fix specific keys that are not correct types
+        config["all"] = reparse config["all"]
+
+        config
+      end
+
+      def reparse val
+        require "yaml"
+        if val.is_a? String then
+          YAML.load val
+        else
+          val
+        end
       end
 
       ##
--- test/test_sanity.rb
+++ test/test_sanity.rb
@@ -1,7 +1,43 @@
 require "minitest/autorun"
+require "cc/engine/flog"
 
 class TestSanity < Minitest::Test
-  def test_sanity
-    flunk "not sane yet"
+  def config subconfig = nil
+    base = {
+            "enabled"       => true,
+            "include_paths" => ["."],
+           }
+    if subconfig then
+      base.merge "config" => subconfig
+    else
+      base
+    end
+  end
+
+  def assert_init exp_all, cfg = nil
+    root = "."
+    exp = ["./lib/cc/engine/flog.rb", "./test/test_sanity.rb"]
+    ccflog = CC::Engine::Flog.new(root, config(cfg))
+    assert_equal exp_all, ccflog.config["all"]
+    assert_equal exp, ccflog.files
+
+    exp_conf = {
+                "include_paths" => ["."],
+                "all" => exp_all,
+               }
+    assert_equal exp_conf, ccflog.config
+
+  end
+
+  def test_initialize_cc_bugs
+    # https://github.com/codeclimate/codeclimate-yaml/issues/38
+    # https://github.com/codeclimate/codeclimate-yaml/issues/39
+
+    assert_init false                   # no config--expected and good
+    assert_init true,  "all" => true    # true config
+    assert_init true,  "all" => "true"  # buggy "true" config
+    assert_init false, "all" => false   # false config
+    assert_init false, "all" => "false" # buggy "false" config
+    assert_init false, ""               # buggy "" config
   end
 end
